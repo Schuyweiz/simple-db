73e7afdfb372daca746d6a939e6dbe476b982d8b
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2024-01-03 17:24:16 +0300

    read message from console and exit using meta command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("db > ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        // creating an empty string object in the heap. Can be mutated to store user input.
+        let mut user_input = String::new();
+        // takes user input as a mutable reference to mutate it - write user input.
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+        match user_input.trim() {
+            ".exit" => {
+                println!("Exiting...");
+                break;
+            }
+            _ => { println!("Unknown command.") }
+        }
+    }
 }
