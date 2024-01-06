b952281c197937d8e8a9f590df53120c8168dc00
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2024-01-06 18:54:25 +0300

    rewrite row serialization/deserialization to serde lib

--- src/storage/constant.rs
+++ src/storage/constant.rs
@@ -3,10 +3,6 @@ pub const USER_NAME_SIZE: usize = 32;
 pub const EMAIL_SIZE: usize = 255;
 pub const ID_SIZE: usize = std::mem::size_of::<u32>();
 pub const ROW_SIZE: usize = USER_NAME_SIZE + EMAIL_SIZE + ID_SIZE;
-pub const ID_OFFSET: usize = 0;
-pub const EMAIL_OFFSET: usize = ID_OFFSET + ID_SIZE;
-pub const USER_NAME_OFFSET: usize = EMAIL_OFFSET + EMAIL_SIZE;
-
 // table constants
 pub const PAGE_SIZE: usize = 4096;
 
--- src/storage/row.rs
+++ src/storage/row.rs
@@ -1,10 +1,9 @@
-use crate::storage::constant::{EMAIL_OFFSET, ID_OFFSET, USER_NAME_OFFSET};
-use crate::storage::constant::{EMAIL_SIZE, ID_SIZE, ROW_SIZE, USER_NAME_SIZE};
+use crate::storage::constant::ROW_SIZE;
 use anyhow::Result;
-use std::convert::TryInto;
+use serde::{Deserialize, Serialize};
 
 //todo: enforce char count for the string smh
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Row {
     id: u32,
     email: String,
@@ -20,47 +19,21 @@ impl Row {
         }
     }
 
+    //todo: custom serializer and deserializer are a pain for the time being, so using this hack for now
     pub fn serialize(&self) -> Result<Vec<u8>> {
-        let mut bytes: Vec<u8> = Vec::new();
-        bytes.extend(&self.id.to_ne_bytes());
-        bytes.extend(Self::serialize_string(&self.email, EMAIL_SIZE));
-        bytes.extend(Self::serialize_string(&self.user_name, USER_NAME_SIZE));
-
-        Ok(bytes)
+        serde_json::to_vec(self)
+            .map_err(|err| anyhow::Error::msg(err))
+            .map(|mut bytes| {
+                bytes.resize(ROW_SIZE, 0);
+                bytes
+            })
     }
 
     pub fn deserialize(bytes: &[u8]) -> Result<Row> {
-        if bytes.len() != ROW_SIZE {
-            return Err(anyhow::Error::msg("Bytes size doesnt match target size."));
-        }
-
-        // we serialize as ne_bytes, probably should deserialize as well in ne
-        let id = u32::from_ne_bytes(
-            bytes[ID_OFFSET..ID_SIZE]
-                .try_into()
-                .expect("Error when converting bytes to id"),
-        );
-
-        //todo: problems with order might arise, need to think how to bind it to fields order
-        let email = Self::deserialize_string(bytes, EMAIL_OFFSET, EMAIL_SIZE);
-        let user_name = Self::deserialize_string(bytes, USER_NAME_OFFSET, USER_NAME_SIZE);
-
-        Ok(Self::new(id, user_name, email))
-    }
-
-    fn serialize_string(str: &String, target_sie: usize) -> Vec<u8> {
-        let mut str_bytes = str.clone().into_bytes();
-        str_bytes.resize(target_sie, 0);
-        str_bytes
-    }
-
-    fn deserialize_string(bytes: &[u8], position_start: usize, target_size: usize) -> String {
-        let offset = bytes[position_start..position_start + target_size]
+        let offset = bytes[0..ROW_SIZE]
             .iter()
             .position(|&byte| byte == 0u8)
-            .unwrap_or(target_size);
-
-        String::from_utf8(bytes[position_start..position_start + offset].to_vec())
-            .expect("Invalid UTF-8 sequence in byte array.")
+            .unwrap_or(ROW_SIZE);
+        serde_json::from_slice(&bytes[0..offset]).map_err(|err| anyhow::Error::msg(err))
     }
 }
